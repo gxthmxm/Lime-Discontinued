04824997145b29693b340fbf18ce0982b9297802
EvenDev <even.flatabo@gmail.com>
2019-04-30 15:28:40 +0200

    Fixed storyboards

--- Lime/HomeViewController.h
+++ Lime/HomeViewController.h
@@ -11,7 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface HomeViewController : UIViewController
-@property (weak, nonatomic) IBOutlet WKWebView *webView;
+@property (strong, nonatomic) IBOutlet WKWebView *webView;
 @end
 
 NS_ASSUME_NONNULL_END
--- Lime/InstalledViewController.h
+++ Lime/InstalledViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface InstalledViewController : UIViewController
+@interface InstalledViewController : UITableViewController
 
 @end
 
