93b7405fe0becc08125846e9fd3a062b6909f5a6
ArtikusHG <artiomoleynic@gmail.com>
2019-04-30 18:10:29 +0300

    add a Makefile

---
+++ Lime/LMDPKGParser.h
@@ -0,0 +1,17 @@
+//
+//  LMDPKGParser.h
+//  Lime
+//
+//  Created by ArtikusHG on 4/30/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LMDPKGParser : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lime/LMDPKGParser.m
@@ -0,0 +1,13 @@
+//
+//  LMDPKGParser.m
+//  Lime
+//
+//  Created by ArtikusHG on 4/30/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+#import "LMDPKGParser.h"
+
+@implementation LMDPKGParser
+
+@end
