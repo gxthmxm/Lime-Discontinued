c7fe696cfea15331b2a991ea6169fb9a573f291e
EvenDev <even.flatabo@gmail.com>
2019-05-02 09:30:35 +0200

    installation page

---
+++ Lime/InstallationController.h
@@ -0,0 +1,18 @@
+//
+//  ChangesViewController.h
+//  Lime
+//
+//  Created by ArtikusHG on 4/30/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InstallationController : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *actionButton;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Lime/InstallationController.m
@@ -0,0 +1,25 @@
+//
+//  ChangesViewController.m
+//  Lime
+//
+//  Created by ArtikusHG on 4/30/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+#import "InstallationController.h"
+
+@interface InstallationController ()
+
+@end
+
+@implementation InstallationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)buttonPressed:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
