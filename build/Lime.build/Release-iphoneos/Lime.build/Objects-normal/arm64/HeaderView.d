c8f735a73bef91f0bbf3441608cd9a8107f0d914
EvenDev <even.flatabo@gmail.com>
2019-05-21 16:23:58 +0200

    fixed twitter cells

--- build/Lime.build/Release-iphoneos/Lime.build/Objects-normal/arm64/HeaderView.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/evendev/Documents/GitHub/Lime/Lime/HeaderView.m \
-  /Users/evendev/Documents/GitHub/Lime/Lime/HeaderView.h
--- build/Lime.build/Release-iphoneos/Lime.build/Objects-normal/arm64/SourcesViewController.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/evendev/Documents/GitHub/Lime/Lime/SourcesViewController.m \
-  /Users/evendev/Documents/GitHub/Lime/Lime/SourcesViewController.h \
-  /Users/evendev/Documents/GitHub/Lime/Lime/Settings.h
