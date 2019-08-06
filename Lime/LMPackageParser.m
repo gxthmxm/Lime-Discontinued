cde884067c20f33bb133f2fa89beae8c2e86cbce
ArtikusHG <artiomoleynic@gmail.com>
2019-08-06 15:53:17 +0300

    start new parser

---
+++ Lime/LMPackageParser.m
@@ -0,0 +1,65 @@
+//
+//  LMPackageParser.m
+//  Lime
+//
+//  Created by ArtikusHG on 7/25/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+#import "LMPackageParser.h"
+#import "LimeHelper.h"
+@import UIKit;
+
+@implementation LMPackageParser
+
+- (instancetype)initWithFilePath:(NSString *)filePath {
+    
+    // TOREMOVE
+    self = [super init];
+    LMPackage *package = [[LMPackage alloc] init];
+    FILE *f = fopen([filePath UTF8String], "r");
+    char str[1024];
+    NSMutableDictionary *mutablePackages = [[NSMutableDictionary alloc] init];
+    
+    NSDictionary *customPropertiesDict = @{
+        @"package":@"identifier",
+        @"description":@"desc",
+        @"icon":@"iconPath",
+        @"depiction":@"depictionURL",
+        @"tag":@"tags",
+        @"depends":@"dependencies",
+        @"installed-size":@"installedSize",
+        @"sileodepiction":@"sileoDepiction"
+    };
+    
+    while(fgets(str, sizeof(str), f) != NULL) {
+        if(strlen(str) < 2) { // a line THAT short is obviously a newline, and we wanna go to the next package if so
+            [mutablePackages setObject:package forKey:package.identifier];
+            // reset it
+            package = nil;
+            package = [[LMPackage alloc] init];
+            //break;
+        } else if(strstr(str, ": ")) {
+            NSString *line = [[NSString stringWithCString:str encoding:NSUTF8StringEncoding] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+            NSArray *lineArray = [line componentsSeparatedByString:@": "]; // Separate the line into the key and the value
+            // initialize the key as the lowercase dpkg key (lowercase because see next comment)
+            NSString *key = [[lineArray objectAtIndex:0] lowercaseString];
+            // LMPackage has custom property names (e.g. description would be desc, dependencies would be depends etc.) so if there is a custom property name set for the current key in our dictionary we change the key
+            if([customPropertiesDict objectForKey:key]) key = [customPropertiesDict objectForKey:key];
+            // the value (most useless comment in the world)
+            NSString *value = [lineArray objectAtIndex:1];
+            
+            if(key && value && [package respondsToSelector:NSSelectorFromString(key)]) {
+                [package setValue:value forKey:key];
+            }
+        }
+    }
+    
+    fclose(f);
+    self.packages = [mutablePackages copy];
+    //UIAlertView *a = [[UIAlertView alloc] initWithTitle:@"a" message:[NSString stringWithFormat:@"%@",self.packages] delegate:nil cancelButtonTitle:@"a" otherButtonTitles:nil];
+    //[a show];
+    return self;
+}
+
+@end
