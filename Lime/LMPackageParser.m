e9caaddefc838531c55794e07d1dd9f1332f751f
pixelomer <37103802+pixelomer@users.noreply.github.com>
2019-08-09 13:02:31 +0300

    An attempt to improve the parser. Not tested

--- Lime/LMPackageParser.m
+++ Lime/LMPackageParser.m
@@ -32,9 +32,10 @@
         @"installed-size":@"installedSize",
         @"sileodepiction":@"sileoDepiction"
     };
-    
+	
+    NSString *lastKey = nil;
     while(fgets(str, sizeof(str), f) != NULL) {
-        if(strlen(str) < 2 && ![package.identifier hasPrefix:@"cy+"] && ![package.identifier hasPrefix:@"gsc."]) { // a line THAT short is obviously a newline, and we wanna go to the next package and add the current one if so; also we don't add packages prefixed with gsc and cy+ 
+        if(!str[1] && ![package.identifier hasPrefix:@"cy+"] && ![package.identifier hasPrefix:@"gsc."]) { // a line THAT short is obviously a newline, and we wanna go to the next package and add the current one if so; also we don't add packages prefixed with gsc and cy+
             if(package.name.length < 1) package.name = package.identifier;
             if(package.iconPath.length > 0) package.iconPath = [package.iconPath stringByReplacingOccurrencesOfString:@"file://" withString:@""];
             else {
@@ -48,26 +49,29 @@
             // reset it
             package = nil;
             package = [[LMPackage alloc] init];
+            lastKey = nil;
             //break;
         } else {
             NSString *line = [[NSString stringWithCString:str encoding:NSUTF8StringEncoding] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
-            if(strstr(str, ": ")) {
-                NSArray *lineArray = [line componentsSeparatedByString:@": "]; // Separate the line into the key and the value
+            if([[NSCharacterSet whitespaceCharacterSet] characterIsMember:[line characterAtIndex:0]]) {
+            	// multiline descriptions
+                NSString *nextLine = [line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]; // remove 4 spaces
+                NSString *oldValue = [package valueForKey:lastKey];
+                [package setValue:[NSString stringWithFormat:@"%@%@", (oldValue ? [oldValue stringByAppendingString:@"\n"] : @""), nextLine] forKey:lastKey];
+            }
+            else if([line containsString:@": "]) {
+                NSMutableArray *lineArray = [line componentsSeparatedByString:@": "].mutableCopy; // Separate the line into the key and the value
                 // initialize the key as the lowercase dpkg key (lowercase because see next comment)
                 NSString *key = [[lineArray objectAtIndex:0] lowercaseString];
                 // LMPackage has custom property names (e.g. description would be desc, dependencies would be depends etc.) so if there is a custom property name set for the current key in our dictionary we change the key
                 if([customPropertiesDict objectForKey:key]) key = [customPropertiesDict objectForKey:key];
                 // the value (most useless comment in the world)
-                NSString *value = [lineArray objectAtIndex:1];
+                [lineArray removeObjectAtIndex:0];
+                NSString *value = [lineArray componentsJoinedByString:@": "];
                 if(key && value && [package respondsToSelector:NSSelectorFromString(key)]) {
-                    [package setValue:value forKey:key];
+                    [package setValue:value forKey:(lastKey = key)];
                 }
-            // multiline descriptions
-            } else if([line rangeOfString:@"    "].location != NSNotFound) {
-                NSString *descriptionLine = [line substringFromIndex:4]; // remove 4 spaces
-                descriptionLine = [@"\n" stringByAppendingString:descriptionLine];
-                package.desc = [package.desc stringByAppendingString:descriptionLine];
-            }
+			}
         }
     }
     
